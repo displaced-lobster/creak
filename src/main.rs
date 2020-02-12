7d5c50b3612a3ade1eb111a05921b63451ea28dc
Richard Mills <scripts.richard@gmail.com>
2020-02-11 19:48:38 -0700

    add desktop notification

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::io;
 use std::io::Write;
 use std::{thread, time};
+use notify_rust::Notification;
 
 #[macro_use]
 extern crate clap;
 
 fn main() {
+    let duration = 1;
     let matches = clap_app!(creak =>
         (version: "0.1.0")
         (author: "Richard Mills <scripts.richard@gmail.com>")
@@ -13,32 +15,31 @@ fn main() {
         (@arg verbose: -d "Set the level of verbosity")
     ).get_matches();
     let _verbose = matches.is_present("verbose");
-    let duration = time::Duration::from_secs(15);
+    let timer = time::Duration::from_secs(duration);
 
+    print!("\x1B[2J");
     println!("Starting standing routine to run every 15 minutes");
 
     loop {
         for _ in 0..60 {
-            thread::sleep(duration);
+            thread::sleep(timer);
             print!(".");
             io::stdout().flush().expect("Failed to flush");
         }
 
         let mut s = String::new();
         let now = time::Instant::now();
-        print!("\nHave you stood up? (Y/y) ");
 
-        loop {
-            io::stdout().flush().expect("Failed to flush");
-            io::stdin().read_line(&mut s).expect("Failed to read user input");
+        Notification::new()
+            .summary("Creak")
+            .body("Time to stand up.")
+            .show()
+            .unwrap();
+        print!("\nHave you stood up?");
 
-            let c = s.chars().next().unwrap();
+        io::stdout().flush().expect("Failed to flush");
+        io::stdin().read_line(&mut s).expect("Failed to read user input");
 
-            if c == 'y' || c == 'Y' || c == '\n' {
-                break;
-            }
-            print!("Invalid input (Y/y): ");
-        }
         print!("\x1B[2J");
         println!("{:?} elapsed after timer expired", now.elapsed());
         println!("Starting next 15 minute timer");
