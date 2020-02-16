2012c98d4048c8907d972795060c417b8e978dac
Richard Mills <scripts.richard@gmail.com>
2020-02-16 13:17:20 -0700

    generate summary chart

--- src/terminal.rs
+++ src/terminal.rs
@@ -2,6 +2,7 @@ extern crate termion;
 
 use std::io;
 use std::io::Write;
+use termion::color;
 
 pub struct Terminal();
 
@@ -23,5 +24,29 @@ impl Terminal {
         io::stdin()
             .read_line(&mut s)
             .expect("Failed to read user input");
+    }
+
+    pub fn generate_chart(values: Vec<u64>, threshold: u64) {
+        let (_height, width) = termion::terminal_size().expect("Failed to get terminal size");
+        let mut max = 0;
+
+        for v in &values {
+            if v > &max {
+                max = *v;
+            }
+        }
+
+        for v in values {
+            let n = (((v as f64) / (max as f64) * (width as f64)) as usize) * 2;
+            let c;
+
+            if v > threshold {
+                c = format!("{}", color::Fg(color::Red));
+            } else {
+                c = format!("{}", color::Fg(color::Green))
+            }
+
+            println!("{}{}", c, "●".repeat(n));
+        }
     }
 }
